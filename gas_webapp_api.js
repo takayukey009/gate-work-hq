e882489087c953daea27f0982db5f7cef0c1f84b
takayukey009 <takayuki.togawa@gate-agency.com>
2026-06-10 14:28:48 +0900

    fix: Revert Gemini API config keys to camelCase

--- gas_webapp_api.js
+++ gas_webapp_api.js
@@ -750,8 +750,8 @@ function callGeminiVision(base64Image, mimeType) {
         ]
       }
     ],
-    generation_config: {
-      response_mime_type: "application/json"
+    generationConfig: {
+      responseMimeType: "application/json"
     }
   };
   
